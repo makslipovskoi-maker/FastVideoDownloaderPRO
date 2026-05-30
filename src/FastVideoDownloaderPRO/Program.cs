4b983491e7aba039c4c7ed7c7b4f6ef9ddfc1395
makslipovskoi-maker <maks.lipovskoi@gmail.com>
2026-05-30 11:34:44 +0300

    Fix WPF application startup

--- src/FastVideoDownloaderPRO/Program.cs
+++ src/FastVideoDownloaderPRO/Program.cs
@@ -1,9 +1,14 @@
+using System;
+using System.Windows;
+
 namespace FastVideoDownloaderPRO;
 
 internal static class Program
 {
+    [STAThread]
     public static void Main()
     {
-        new MainWindow().ShowDialog();
+        var app = new Application();
+        app.Run(new MainWindow());
     }
 }
